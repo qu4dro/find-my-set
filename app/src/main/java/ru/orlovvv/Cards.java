058a1915376ad9bfa986f1f1b1c5a68b3e11ad2c
qu4dro <qu4dro.sl@gmail.com>
2020-04-19 13:42:59 +0800

    classes added

---
+++ app/src/main/java/ru/orlovvv/Cards.java
@@ -0,0 +1,12 @@
+package ru.orlovvv;
+
+import java.util.ArrayList;
+
+public class Cards {
+
+    ArrayList<Card> cards;
+
+    public ArrayList<Card> getCards() {
+        return cards;
+    }
+}
---
+++ app/src/main/java/ru/orlovvv/Response.java
@@ -0,0 +1,9 @@
+package ru.orlovvv;
+
+import java.util.ArrayList;
+
+public class Response {
+    String status;
+    int token, points;
+    ArrayList<Card> cards;
+}
